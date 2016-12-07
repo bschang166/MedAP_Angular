57929e6982470ee0d25b48d8370d4d84a1732162
bnchang <bschang166@gmail.com>
2016-12-07 16:46:27 -0500

    typeahead tests

--- src/app/common/typeahead/typeahead.component.spec.js
+++ src/app/common/typeahead/typeahead.component.spec.js
@@ -8,45 +8,89 @@ describe('Typeahead component', () => {
 
 // ---------- Unit Tests -------------------------------------------------------
   describe('unit tests', () => {
+    let $timeout;
+
     let scope;
     let element;
     let ctrl;
-    let deferred;
 
-    beforeEach(inject(($rootScope, _$q_, $compile) => {
-      deferred = _$q_.defer();
+    let debounce = 300;
+    let input;
+    beforeEach(inject(($rootScope, $compile, _$timeout_) => {
+      $timeout = _$timeout_;
 
       scope = $rootScope.$new();
-      element = $compile('<typeahead></typeahead>')(scope);
+      scope.onChange = jasmine.createSpy('onChange');
+      element = $compile('<typeahead display-records="displayRecords" on-change="onChange($event)"></typeahead>')(scope);
       ctrl = element.controller('typeahead');
+
+      input = element.find('input');
+      scope.$digest();
     }));
 
-  });
+    it('should call onChange when input changes', () => {
+      input.val('test me').triggerHandler('change');
+      $timeout.flush(debounce);
+      expect(scope.onChange).toHaveBeenCalledWith({term: 'test me'});
+    });
 
+    it('should only call onChange until input changes to a distinct value', () => {
+      input.val('test me').triggerHandler('change');
+      $timeout.flush(debounce);
+      expect(scope.onChange.calls.count()).toEqual(1);
 
-// ---------- Shallow Unit Tests ------------------------------------------------
-  describe('shallow tests', () => {
-    let $componentController;
-    let scope;
-    let element;
-    let ctrl;
+      input.val('test me').triggerHandler('change');
+      $timeout.flush(debounce);
+      expect(scope.onChange.calls.count()).toEqual(1);
 
-    let deferred;
+      input.val('test me again').triggerHandler('change');
+      $timeout.flush(debounce);
+      expect(scope.onChange.calls.count()).toEqual(2);
+    });
 
-    let onChange = function(){};
-    beforeEach(inject(($rootScope, _$componentController_, _$q_, $compile) => {
-      deferred = _$q_.defer();
+    it('should call onChange with search term on input focus', () => {
+      ctrl.searchTerm = 'test search term';
+      input.triggerHandler('focus');
+      expect(scope.onChange).toHaveBeenCalledWith({term: 'test search term'});
+    });
 
-      scope = $rootScope.$new();
-      $componentController = _$componentController_;
+    it('should debounce input change events', () => {
+      input.val('test me').triggerHandler('change');
+      $timeout.flush(debounce-1);
+      expect(scope.onChange).not.toHaveBeenCalled();
+      input.val('test me again').triggerHandler('change');
+      $timeout.flush(debounce);
+      expect(scope.onChange).toHaveBeenCalled();
+    });
 
-      ctrl = $componentController('typeahead', {
-        $scope: scope,
-      }, {
-        onChange: onChange,
-        debounceTime: 300
-      });
-    }));
+    it('should not display records until input focus', () => {
+      scope.displayRecords = ['1', '2'];
+      scope.$digest();
+      expect(element.find('li').length).toEqual(0);
+    })
+
+    it('should display records in array order on input focus', () => {
+      scope.displayRecords = ['test item 1', 'test item 2'];
+      input.triggerHandler('focus');
+      let items = element.find('li');
+      expect(items.eq(0).text()).toContain('test item 1');
+      expect(items.eq(1).text()).toContain('test item 2');
+    });
+
+    it('should not display records on input blur', () => {
+      scope.displayRecords = ['test item 1', 'test item 2'];
+      input.triggerHandler('focus');
+      input.triggerHandler('blur');
+      expect(element.find('li').length).toEqual(0);
+    });
+
+    it('should display "Empty list" if no display records are given', () => {
+      scope.displayRecords = [];
+      input.triggerHandler('focus');
+      let items = element.find('li');
+      expect(items.length).toEqual(1);
+      expect(items.eq(0).text()).toContain('Empty list');
+    });
 
   });
 
